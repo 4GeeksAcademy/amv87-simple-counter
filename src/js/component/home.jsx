90bff7c98facd4668d072a4dad0291857377f7ed
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-11-03 17:02:18 +0000

    added working setinterval function

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,22 +1,36 @@
 import React from "react";
-import Counter from "./counter";
-
-
+import ReactDOM from 'react-dom';
+import Box from "./box";
 
 const SimpleCounter = (props) => {
 	return (
 		<div className="container my-5">
 			<div className="d-flex flex-row justify-content-evenly rounded align-items-center" style={{width: '80rem', height:'16rem', backgroundColor: 'rgb(9, 9, 9)'}}>
-				<Counter number={<i className="far fa-clock"></i>} />
-				<Counter number={props.secondHundredThousands} />
-				<Counter number={props.secondTenThousands} />
-				<Counter number={props.secondThousands} />
-				<Counter number={props.secondHundreds} />
-				<Counter number={props.secondTens} />
-				<Counter number={props.secondUnits} />
+				<Box number={<i className="far fa-clock"></i>} />
+				<Box number={props.secondHundredThousands} />
+				<Box number={props.secondTenThousands} />
+				<Box number={props.secondThousands} />
+				<Box number={props.secondHundreds} />
+				<Box number={props.secondTens} />
+				<Box number={props.secondUnits} />
 			</div>
 		</div>
 	);
 };
 
 export default SimpleCounter;
+
+let counter = 0;
+setInterval (function(){
+  const first = Math.floor(counter/1) % 10;
+  const second = Math.floor(counter/10) % 10;
+  const third = Math.floor(counter/100) % 10;
+  const fourth = Math.floor(counter/1000) % 10;
+  const fifth = Math.floor(counter/10000) % 10;
+  const sixth = Math.floor(counter/100000) % 10;
+  counter++;
+  ReactDOM.render(
+    <SimpleCounter secondUnits={first} secondTens={second} secondHundreds={third} secondThousands={fourth} secondTenThousands={fifth} secondHundredThousands={sixth}/>,
+    document.querySelector('#app')
+  );
+}, 1000);
