b41c77ee7a8a87ba819c81af2793df1a4b2c7c4b
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-11-03 17:01:23 +0000

    added box.jsx and removed counter.jsx

--- src/js/component/box.jsx
+++ src/js/component/box.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Counter = (props) => {
+const Box = (props) => {
 	return (
 		<div className="d-flex align-items-center rounded justify-content-center border border-dark" style={{width: '8rem', height:'12rem', backgroundColor: 'rgb(22, 22, 22)'}}>
 				<h1 className="text-white display-1 fw-bold">{props.number}</h1>
@@ -8,4 +8,4 @@ const Counter = (props) => {
 	);
 };
 
-export default Counter;
+export default Box;
